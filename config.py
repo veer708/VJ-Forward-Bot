ad32dc85a194cb1d70ed7be4952e62232a27437d
veer708 <veerahirwal51@gmail.com>
2025-07-28 12:16:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     BOT_SESSION = environ.get("BOT_SESSION", "Anushka") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://jackey172363:KeAhgj2viyWoHrxn@cluster0.s7ydqcf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vinod7u")
-    BOT_OWNER = int(environ.get("BOT_OWNER", "7081893529"))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "8084341411"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
